3961f8bc0b87f2af3e19d7a4b0be0a69cb9580e6
Yasser Nascimento <dev@yasser.email>
2023-08-01 16:56:45 -0300

    add codex

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,14 @@
 <template>
-  <p>home page</p>
+  <div>
+    <cdx-button action="progressive" weight="primary">Click me</cdx-button>
+  </div>
 </template>
+
+<script>
+import "../base.css"
+
+import { defineComponent } from 'vue'
+import { CdxButton } from '@wikimedia/codex'
+
+export default defineComponent({ components: { CdxButton } })
+</script>
